643ece5a5308259a83ede3f44abaf6b31802bcf1
Josael Araujo <josaelgon0123@gmail.com>
2026-01-10 19:51:16 -0300

    alterações

---
+++ 01movimento.js
@@ -0,0 +1,9 @@
+// Aos inicial
+AOS.init({duration: 1000, once:true});
+
+// Alternar menu
+
+function toggleMenu(){
+    const nav = document.getElementById("nav-menu");
+    nav.classList.toggle("active");
+}
