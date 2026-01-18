eddc5d319d86e37c777de1cdc78fd2eb93629bc1
Josael Araujo <josaelgon0123@gmail.com>
2026-01-18 19:09:50 -0300

    alterações

--- 01movimento.js
+++ 01movimento.js
@@ -7,3 +7,19 @@ function toggleMenu(){
     const nav = document.getElementById("nav-menu");
     nav.classList.toggle("active");
 }
+
+//Animação parallax
+const animItem = document.querySelector('.mouse-anim');
+
+document.addEventListener('mousemove', (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 1000;
+    const y = (e.clientY / window.innerHeight - 0.5) * 1000;
+
+    anime({
+        targets: animItem,
+        translateX: x,
+        translateY: y,
+        easing: 'easeOutQuad',
+        duration: 80
+    });
+});
